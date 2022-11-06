e5d858e8d97fcd5c90431f3d184519cc311be081
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-11-06 16:00:58 +0700

    MySQL: Initial commit
    This is the initial commit of the C++ MySQL project.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ MySQL.cpp
@@ -0,0 +1,194 @@
+
+#define KEEP_CPPMYSQL_HELPER_MACROS
+#include "MySQL.h"
+
+#ifndef __cold
+#define __cold __attribute__((__cold__))
+#endif
+
+#ifndef __hot
+#define __hot __attribute__((__hot__))
+#endif
+
+namespace CppMySQL {
+
+__cold
+MySQL::~MySQL(void) noexcept
+{
+	if (conn_)
+		Close();
+}
+
+__cold
+int MySQL::Init(void) noexcept
+{
+	conn_ = mysql_init(NULL);
+	if (!conn_) {
+		SetErr("Failed to perform mysql_init()");
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+__cold
+int MySQL::Connect(void) noexcept
+{
+	MYSQL *ret;
+
+	ret = mysql_real_connect(conn_, host_, user_, passwd_, dbname_, port_,
+				 NULL, 0);
+	if (unlikely(!ret)) {
+		SetMySQLErrFallback("Failed to perform mysql_real_connect()");
+		return -1;
+	}
+
+	assert(ret == conn_);
+	return 0;
+}
+
+__hot
+const char *MySQL::Error(void) noexcept
+{
+	if (err_[0])
+		return err_;
+
+	if (conn_) {
+		const char *ret = mysql_error(conn_);
+		if (ret && ret[0])
+			return ret;
+	}
+
+	return nullptr;
+}
+
+__hot
+MySQLResult *MySQL::StoreResult(void) noexcept
+{
+	MySQLResult *ret;
+	MYSQL_RES *res;
+
+	res = mysql_store_result(conn_);
+	if (unlikely(!res)) {
+		ClearErr();
+		return nullptr;
+	}
+
+	ret = new(std::nothrow) MySQLResult(this, res);
+	if (unlikely(!ret)) {
+		mysql_free_result(res);
+		SetErr("Cannot allocate memory for MySQLResult");
+		return nullptr;
+	}
+
+	return ret;
+}
+
+__hot
+MYSQL_ROW MySQLResult::FetchRow(void) noexcept
+{
+	MYSQL_ROW ret;
+
+	ret = mysql_fetch_row(res_);
+	if (unlikely(!ret)) {
+		mysql_->ClearErr();
+		if (unlikely(mysql_->Error()))
+			return nullptr;
+
+		return MYSQL_ROW_END_PTR;
+	}
+
+	return ret;
+}
+
+__hot
+MySQLStmt *MySQL::Prepare(size_t nr_bind, const char *q, size_t qlen) noexcept
+{
+	MYSQL_BIND *bind = nullptr;
+	MYSQL_STMT *stmt;
+	MySQLStmt *ret;
+	int tmp;
+
+	stmt = mysql_stmt_init(conn_);
+	if (unlikely(!stmt))
+		return nullptr;
+
+	tmp = mysql_stmt_prepare(stmt, q, qlen);
+	if (unlikely(tmp)) {
+		const char *err = mysql_stmt_error(stmt);
+
+		if (err && *err)
+			SetErr("%s", err);
+		goto err;
+	}
+
+	if (nr_bind > 0) {
+		bind = static_cast<MYSQL_BIND *>(calloc(nr_bind, sizeof(*bind)));
+		if (unlikely(!bind)) {
+			SetErr("Cannot allocate memory for MYSQL_BIND");
+			goto err;
+		}
+	}
+
+	ret = new(std::nothrow) MySQLStmt(this, stmt, bind);
+	if (unlikely(!ret)) {
+		SetErr("Cannot allocate memory for MySQLStmt");
+		goto err;
+	}
+
+	return ret;
+
+err:
+	free(bind);
+	mysql_stmt_close(stmt);
+	return nullptr;
+}
+
+__hot
+const char *MySQLStmt::Error(void) noexcept
+{
+	if (err_[0])
+		return err_;
+
+	if (stmt_) {
+		const char *ret = mysql_stmt_error(stmt_);
+		if (ret && ret[0])
+			return ret;
+	}
+
+	return nullptr;
+}
+
+MySQLStmtResult *MySQLStmt::StoreResult(void) noexcept
+{
+	MySQLStmtResult *ret = nullptr;
+	MYSQL_BIND *bind = nullptr;
+	MYSQL_RES *res;
+	size_t nfields;
+
+	res = mysql_stmt_result_metadata(stmt_);
+	if (unlikely(!res))
+		return nullptr;
+
+	nfields = mysql_num_fields(res);
+	bind = static_cast<MYSQL_BIND *>(calloc(nfields, sizeof(*bind)));
+	if (unlikely(!bind)) {
+		SetErr("Cannot allocate memory for MYSQL_BIND");
+		goto err;
+	}
+
+	ret = new(std::nothrow) MySQLStmtResult(stmt_, res, bind);
+	if (unlikely(!ret)) {
+		SetErr("Cannot allocate memory for MySQLStmtResult");
+		goto err;
+	}
+
+	return ret;
+
+err:
+	free(bind);
+	mysql_free_result(res);
+	return nullptr;
+}
+
+} /* namespace CppMySQL */
---
+++ MySQL.h
@@ -0,0 +1,322 @@
+
+#ifndef CPP_MYSQL_H
+#define CPP_MYSQL_H
+
+#include <mysql/mysql.h>
+
+#include <new>
+#include <cstdio>
+#include <cerrno>
+#include <cstdint>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+#ifndef likely
+#define likely(COND) __builtin_expect(!!(COND), 1)
+#endif
+
+#ifndef unlikely
+#define unlikely(COND) __builtin_expect(!!(COND), 0)
+#endif
+
+#ifndef noinline
+#define noinline __attribute__((__noinline__))
+#endif
+
+namespace CppMySQL {
+
+#define SetErr(...)					\
+do {							\
+	snprintf(err_, sizeof(err_), __VA_ARGS__);	\
+} while (0)
+
+#define SetMySQLErrFallback(...)			\
+do {							\
+	const char *___err = mysql_error(conn_);	\
+							\
+	if (___err && ___err[0])			\
+		SetErr("%s", ___err);			\
+	else						\
+		SetErr(__VA_ARGS__);			\
+} while (0)
+
+
+class MySQLStmt;
+class MySQLResult;
+
+class MySQL {
+public:
+	inline MySQL(const char *host, const char *user = nullptr,
+		     const char *passwd = nullptr, const char *dbname = nullptr,
+		     uint32_t port = 0) noexcept:
+		host_(host),
+		user_(user),
+		passwd_(passwd),
+		dbname_(dbname),
+		port_(port)
+	{
+		ClearErr();
+	}
+
+	~MySQL(void) noexcept;
+	int Init(void) noexcept;
+	int Connect(void) noexcept;
+
+	MySQLStmt *Prepare(size_t nr_bind, const char *q, size_t qlen) noexcept;
+
+	inline MySQLStmt *Prepare(size_t nr_bind, const char *q) noexcept
+	{
+		return Prepare(nr_bind, q, strlen(q));
+	}
+
+	inline unsigned int Errno(void) noexcept
+	{
+		return mysql_errno(conn_);
+	}
+
+	const char *Error(void) noexcept;
+
+	inline int RealQuery(const char *q, unsigned long len) noexcept
+	{	
+		return mysql_real_query(conn_, q, len);
+	}
+
+	inline int RealQuery(const char *q) noexcept
+	{	
+		return mysql_real_query(conn_, q, strlen(q));
+	}
+
+	MySQLResult *StoreResult(void) noexcept;
+
+	inline void Close(void) noexcept
+	{
+		mysql_close(conn_);
+		conn_ = nullptr;
+	}
+
+	inline MYSQL *Conn(void) noexcept
+	{
+		return conn_;
+	}
+
+	inline void ClearErr(void) noexcept
+	{
+		err_[0] = '\0';
+	}
+
+private:
+	MYSQL		*conn_ = nullptr;
+	const char	*host_;
+	const char	*user_;
+	const char	*passwd_;
+	const char	*dbname_;
+	uint16_t	port_ = 0;
+	char		err_[128];
+};
+
+
+#define MYSQL_ROW_END_PTR (reinterpret_cast<MYSQL_ROW>(-1ul))
+#define IS_MYSQL_ROW_END(ARG) ((ARG) == MYSQL_ROW_END_PTR)
+
+class MySQL;
+
+class MySQLResult {
+public:
+	inline MySQLResult(MySQL *mysql, MYSQL_RES *res) noexcept:
+		res_(res),
+		mysql_(mysql)
+	{
+	}
+
+	inline ~MySQLResult(void) noexcept
+	{
+		mysql_free_result(res_);
+	}
+
+	inline uint64_t NumRows(void) noexcept
+	{
+		return mysql_num_rows(res_);
+	}
+
+	MYSQL_ROW FetchRow(void) noexcept;
+
+private:
+	MYSQL_RES	*res_;
+	MySQL		*mysql_;
+};
+
+class MySQLStmtResult {
+public:
+	inline MySQLStmtResult(MYSQL_STMT *stmt, MYSQL_RES *res,
+			       MYSQL_BIND *bind) noexcept:
+		res_(res),
+		stmt_(stmt),
+		bind_(bind)
+	{
+	}
+
+	inline MYSQL_BIND *Bind(size_t i)
+	{
+		return &bind_[i];
+	}
+
+	inline MYSQL_BIND *Bind(size_t i, enum enum_field_types type, void *buf,
+				size_t buflen, bool *is_null = nullptr,
+				size_t *len = nullptr) noexcept
+	{
+		MYSQL_BIND *b = &bind_[i];
+
+		b->buffer_type = type;
+		b->buffer = buf;
+		b->buffer_length = buflen;
+		b->is_null = is_null;
+		b->length = len;
+		return b;
+	}
+
+	inline int StoreResult(void) noexcept
+	{
+		return mysql_stmt_store_result(stmt_);
+	}
+
+	inline int BindResult(void) noexcept
+	{
+		return mysql_stmt_bind_result(stmt_, bind_);
+	}
+
+	inline size_t NumFields(void) noexcept
+	{
+		return mysql_num_fields(res_);
+	}
+
+	inline int Fetch(void) noexcept
+	{
+		return mysql_stmt_fetch(stmt_);
+	}
+
+	inline ~MySQLStmtResult(void) noexcept
+	{
+		free(bind_);
+		mysql_free_result(res_);
+	}
+
+private:
+	MYSQL_RES	*res_;
+	MYSQL_STMT	*stmt_;
+	MYSQL_BIND	*bind_;
+};
+
+class MySQLStmt {
+public:
+	inline MySQLStmt(MySQL *mysql, MYSQL_STMT *stmt):
+		stmt_(stmt),
+		bind_(nullptr),
+		mysql_(mysql)
+	{
+		ClearErr();
+	}
+
+	inline MySQLStmt(MySQL *mysql, MYSQL_STMT *stmt, MYSQL_BIND *bind):
+		stmt_(stmt),
+		bind_(bind),
+		mysql_(mysql)
+	{
+		ClearErr();
+	}
+
+	inline void ClearErr(void) noexcept
+	{
+		err_[0] = '\0';
+	}
+
+	inline MYSQL_BIND *Bind(size_t i)
+	{
+		return &bind_[i];
+	}
+
+	inline ~MySQLStmt(void) noexcept
+	{
+		mysql_stmt_close(stmt_);
+		if (bind_)
+			free(bind_);
+	}
+
+	inline MYSQL_BIND *BindStr(size_t i, enum enum_field_types type,
+				   const char *str, size_t len) noexcept
+	{
+		MYSQL_BIND *b = &bind_[i];
+
+		b->buffer_type = type;
+		b->buffer = const_cast<void *>(static_cast<const void *>(str));
+		b->buffer_length = len;
+		return b;
+	}
+
+	inline MYSQL_BIND *BindStr(size_t i, enum enum_field_types type,
+				   const char *str) noexcept
+	{
+		MYSQL_BIND *b = &bind_[i];
+
+		b->buffer_type = type;
+		b->buffer = const_cast<void *>(static_cast<const void *>(str));
+		b->buffer_length = strlen(str);
+		return b;
+	}
+
+	inline MYSQL_BIND *Bind(size_t i, enum enum_field_types type,
+				void *buf = nullptr, size_t buflen = 0) noexcept
+	{
+		MYSQL_BIND *b = &bind_[i];
+
+		b->buffer_type = type;
+		b->buffer = buf;
+		b->buffer_length = buflen;
+		return b;
+	}
+
+	inline int BindStmt(void) noexcept
+	{
+		return mysql_stmt_bind_param(stmt_, bind_);
+	}
+
+	inline int Execute(void) noexcept
+	{
+		return mysql_stmt_execute(stmt_);
+	}
+
+	inline unsigned int Errno(void) noexcept
+	{
+		return mysql_stmt_errno(stmt_);
+	}
+
+	const char *Error(void) noexcept;
+
+	inline unsigned int MySQLErrno(void) noexcept
+	{
+		return mysql_errno(mysql_->Conn());
+	}
+
+	inline const char *MySQLError(void) noexcept
+	{
+		return mysql_error(mysql_->Conn());
+	}
+
+	MySQLStmtResult *StoreResult(void) noexcept;
+
+private:
+	MYSQL_STMT	*stmt_;
+	MYSQL_BIND	*bind_;
+	MySQL		*mysql_;
+	char		err_[128];
+};
+
+
+#if !defined(KEEP_CPPMYSQL_HELPER_MACROS)
+#undef SetErr
+#undef SetMySQLErrFallback
+#endif
+
+} /* namespace CppMySQL */
+
+#endif /* #ifndef CPP_MYSQL_H */
